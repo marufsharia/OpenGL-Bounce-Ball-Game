0c2a0f4be2032e0d9ace0ca0738f087e6e9a2d33
MarufSharia <marufsharia@gmail.com>
2017-08-11 22:48:41 +0600

    first commit for add main cpp and prototype 1

---
+++ Source/header.h
@@ -0,0 +1,18 @@
+/************************************************************
+// This is simple Bounce Ball Game .
+//  All header file and define macro will be add here.
+*************************************************************/
+
+//All header file
+#include<iostream>
+#include<stdlib.h>
+#include<windows.h>
+#include<time.h>
+#include<string.h>
+#include<math.h>
+#include<conio.h>
+#include<GL/gl.h>
+#include<Gl/glut.h>
+
+
+using namespace std;
